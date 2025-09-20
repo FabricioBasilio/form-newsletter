557674b88ddc5046a9adc67bae8995f39420c622
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-09-20 17:36:53 -0300

    feat: estrutura do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
-      <h1 className="text-blue-500">Olá mundo</h1>
-
-      <Form />
-    </>
+    <main>
+      <h1 className="text-blue-500">Inscreva-se</h1>
+      <p>Assine nossa Newsletter e mantenha-se informado</p>
+      <section>
+        <Form />
+      </section>
+      <p>Ao se inscrever, você passará a receber os nossos e-mails com as melhores dicas, novidades e ofertas.</p>
+    </main>
   );
 }
 
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,28 @@
 function Form() {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="nome">Nome</label>
+        <input type="text" placeholder="Digite o seu nome" id="nome" />
+      </div>
+      <div>
+        <label htmlFor="nome">E-mail</label>
+        <input
+          type="email"
+          placeholder="Insira o seu melhor e-mail"
+          id="email"
+        />
+      </div>
+      <section>
+        <a href="#">Leia os termos</a>
+        <div>
+          <input type="checkbox" id="termos" />
+          <label htmlFor="termos">Concordo com os termos</label>
+        </div>
+      </section>
+      <button type="submit">Cadastrar</button>
+    </form>
+  );
 }
 
 export default Form;
