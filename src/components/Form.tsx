7568e410cfbd51248cc570eec5b8390b84c5d734
Fabricio Basilio <fabriciobasilioo@gmail.com>
2025-09-20 23:01:06 -0300

    feat: validacao dos dados do formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,59 @@
+import { useState } from "react";
+import type { FormEvent } from "react";
+import type { User } from "../types/User";
+import validate from "../utils/validate";
+
 function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const [erros, setErros] = useState<User | null>(null);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    setErros(null);
+
+    const data: User = {
+      name,
+      email,
+      agree,
+    };
+
+    const validateErrors = validate(data);
+
+    console.log(data, validateErrors);
+
+    if (Object.keys(validateErrors).length > 0) {
+      setErros(validateErrors);
+      return;
+    }
+
+    setName("");
+    setEmail("");
+    setAgree(false);
+
+    alert("Obrigado por se inscrever!");
+  }
+
   return (
-    <form className="flex flex-col gap-3">
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
       <div className="flex flex-col">
         <label htmlFor="nome" className="text-sm">
           Nome
         </label>
         <input
           type="text"
+          value={name}
           placeholder="Digite o seu nome"
           id="nome"
           className="bg-white rounded-lg py-2 px-2 text-sm placeholder:text-stone-400 placeholder:text-sm"
+          onChange={(e) => setName(e.target.value)}
         />
+        {erros?.name && (
+          <small className="text-xs text-red-500 mt-1">{erros?.name}</small>
+        )}
       </div>
       <div className="flex flex-col">
         <label htmlFor="nome" className="text-sm">
@@ -18,21 +61,34 @@ function Form() {
         </label>
         <input
           type="email"
+          value={email}
           placeholder="Insira o seu melhor e-mail"
           id="email"
           className="bg-white rounded-lg py-2 px-2 text-sm placeholder:text-stone-400 placeholder:text-sm"
+          onChange={(e) => setEmail(e.target.value)}
         />
+        {erros?.email && (
+          <small className="text-xs text-red-500 mt-1">{erros?.email}</small>
+        )}
       </div>
       <section className="flex flex-col">
         <a href="#" className="text-xs underline mb-2">
           Leia os termos
         </a>
         <div className="flex gap-2 items-center">
-          <input type="checkbox" id="termos" />
-          <label htmlFor="termos" className="text-sm">
+          <input
+            type="checkbox"
+            checked={agree}
+            id="agree"
+            onChange={(e) => setAgree(e.target.checked)}
+          />
+          <label htmlFor="agree" className="text-sm">
             Concordo com os termos
           </label>
         </div>
+        {erros?.agree && (
+          <small className="text-xs text-red-500 mt-1">{erros?.agree}</small>
+        )}
       </section>
       <button
         type="submit"
---
+++ src/utils/validate.ts
@@ -0,0 +1,20 @@
+import type { User } from "../types/User";
+
+type Error = {
+  [key: string]: string;
+};
+
+function validate(data: User) {
+  const errors: Error = {};
+
+  if (!data.name) errors["name"] = "O nome é obrigatório";
+
+  if (!data.email) errors["email"] = "O e-mail é obrigatório";
+
+  if (!data.agree) errors["agree"] = "Você precisa concordar com os termos";
+
+
+  return errors;
+}
+
+export default validate;
